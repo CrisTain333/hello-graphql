c842335d4ea3690aae5ebc97e8fa46d00e579e56
Sukanta Das <arnobh84@gmail.com>
2023-11-13 18:12:42 +0600

    setting up graphql server

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -14,31 +13,26 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
-
 const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
 ];
-
 const resolvers = {
-  Query: {
-    books: () => books,
-  },
+    Query: {
+        books: () => books,
+    },
 };
-
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
-
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 8000 },
+    listen: { port: 8000 },
 });
-
 console.log(`🚀  Server Fire at: ${url}`);
