5409645dfe23d365d7f7790bccde05a90fcb1339
Sukanta Das <arnobh84@gmail.com>
2023-11-14 10:51:48 +0600

    some extra code added

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,14 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './gql/schema/index.js';
+import { resolvers } from './gql/resolvers/index.js';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers
 });
-
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 8000 },
+    listen: { port: 4000 },
 });
 
-console.log(`🚀  Server Fire at: ${url}`);
+console.log(`🚀  Server ready at: ${url}`);
